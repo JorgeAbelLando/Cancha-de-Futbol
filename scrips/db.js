6801d49c84b5330e0290f37f000deb583689a4b6
JorgeAbelLando <landojorgeabel@gmail.com>
2022-03-25 09:35:40 -0300

    Desafio

---
+++ scrips/db.js
@@ -0,0 +1,26 @@
+var galleteria = {
+    "nombre":"galletiteria",
+    "abre":"8:30",
+    "ciera":"20",
+    "stock":[
+        
+        {
+            "codigo":"2",
+            "nombre":"pitusas",
+            "stock":"6",
+            "precio":"80"
+        },
+        {
+            "codigo":"3",
+            "nombre":"boca de dama",
+            "stock":"10",
+            "precio":"50"
+        },
+        {
+            "codigo":"4",
+            "nombre":"rochitas",
+            "stock":"40",
+            "precio":"20"
+        }
+    ]
+}
